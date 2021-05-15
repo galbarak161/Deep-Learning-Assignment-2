a6caeea1d31626d7975cce710663973cd4ca7929
gal barak <galbarak161@gmail.com>
2021-05-15 16:15:19 +0300

    Update optimizer and scheduler

--- Models/GTSRBModel.py
+++ Models/GTSRBModel.py
@@ -13,19 +13,19 @@ from Models.util.Consts import *
 
 class GTSRBModel(nn.Module):
 
-    def __init__(self, modelId, dropout=False, batch_normalization=False, fully_connected_nn=True):
+    def __init__(self, model_id, dropout=False, batch_normalization=False, fully_connected_nn=True):
         super().__init__()
 
-        self.modelId = modelId
+        self.modelId = model_id
         self.lossFunction = torch.nn.CrossEntropyLoss()
 
         self.logSoftMax = nn.LogSoftmax(dim=1)
 
         self.featureExtractor = nn.Sequential(
-            nn.Conv2d(3, out_channels=6, kernel_size=5),
+            nn.Conv2d(in_channels=3, out_channels=6, kernel_size=(5, 5)),
             nn.ReLU(),
             nn.MaxPool2d(2),
-            nn.Conv2d(in_channels=6, out_channels=16, kernel_size=5),
+            nn.Conv2d(in_channels=6, out_channels=16, kernel_size=(5, 5)),
             nn.ReLU(),
             nn.MaxPool2d(2),
         )
@@ -42,18 +42,18 @@ class GTSRBModel(nn.Module):
                 model_id += 1
 
         else:
-            self.classifier.add_module(f'{model_id}', nn.Conv2d(in_channels=16, out_channels=120, kernel_size=4))
+            self.classifier.add_module(f'{model_id}', nn.Conv2d(in_channels=16, out_channels=120, kernel_size=(4, 4)))
             model_id += 1
             if batch_normalization:
                 self.classifier.add_module(f'{model_id}', nn.BatchNorm2d(120))
                 model_id += 1
 
+        self.classifier.add_module(f'{model_id}', nn.ReLU())
+        model_id += 1
+
         if dropout:
             self.classifier.add_module(f'{model_id}', nn.Dropout(0.2))
             model_id += 1
-
-        self.classifier.add_module(f'{model_id}', nn.ReLU())
-        model_id += 1
 
         if fully_connected_nn:
             self.classifier.add_module(f'{model_id}', nn.Linear(120, 84))
@@ -63,31 +63,29 @@ class GTSRBModel(nn.Module):
                 model_id += 1
 
         else:
-            self.classifier.add_module(f'{model_id}', nn.Conv2d(in_channels=120, out_channels=84, kernel_size=1))
+            self.classifier.add_module(f'{model_id}', nn.Conv2d(in_channels=120, out_channels=84, kernel_size=(1, 1)))
             model_id += 1
             if batch_normalization:
                 self.classifier.add_module(f'{model_id}', nn.BatchNorm2d(84))
                 model_id += 1
 
+        self.classifier.add_module(f'{model_id}', nn.ReLU())
+        model_id += 1
+
         if dropout:
             self.classifier.add_module(f'{model_id}', nn.Dropout(0.2))
             model_id += 1
-
-        self.classifier.add_module(f'{model_id}', nn.ReLU())
-        model_id += 1
 
         if fully_connected_nn:
             self.classifier.add_module(f'{model_id}', nn.Linear(84, 43))
             model_id += 1
 
         else:
-            self.classifier.add_module(f'{model_id}', nn.Conv2d(in_channels=84, out_channels=43, kernel_size=1))
+            self.classifier.add_module(f'{model_id}', nn.Conv2d(in_channels=84, out_channels=43, kernel_size=(1, 1)))
             model_id += 1
 
-        self.optimizer = torch.optim.Adam(self.parameters())
-        self.scheduler = torch.optim.lr_scheduler.StepLR(self.optimizer, step_size=3)
-
-        self.dropout = dropout
+        self.optimizer = torch.optim.Adam(self.parameters(), lr=0.001, weight_decay=0.001)
+        self.scheduler = torch.optim.lr_scheduler.StepLR(self.optimizer, step_size=3, gamma=0.8)
 
         self.to(DEVICE)
 
@@ -97,11 +95,11 @@ class GTSRBModel(nn.Module):
         probabilities = self.logSoftMax(class_scores)
         return probabilities
 
-    def countParameters(self) -> int:
+    def count_parameters(self) -> int:
         return sum(p.numel() for p in self.parameters() if p.requires_grad)
 
-    def getPredictions(self, pathToImage):
-        img = Image.open(pathToImage)
+    def get_predictions(self, path_to_image):
+        img = Image.open(path_to_image)
 
         transform = transforms.Compose([
             transforms.Resize((30, 30)),
@@ -118,46 +116,43 @@ class GTSRBModel(nn.Module):
 
             return prediction
 
-    def calculateAccuracy(self, data_set: DataLoader, with_grad=True) -> float:
-
+    def calculate_accuracy(self, data_set: DataLoader, with_grad=True) -> float:
+        self.eval()
         with torch.set_grad_enabled(with_grad):
-            nCorrect = 0
+            n_correct = 0
             total = 0
             for data, labels in data_set:
                 data, labels = data.to(DEVICE), labels.to(DEVICE)
 
                 # calculate output
-                predictionsProbabilities = self(data).squeeze()
+                predictions_probabilities = self(data).squeeze()
 
                 # get the prediction
-                predictions = torch.argmax(predictionsProbabilities, dim=1)
-                nCorrect += torch.sum(predictions == labels).item()
+                predictions = torch.argmax(predictions_probabilities, dim=1)
+                n_correct += torch.sum(predictions == labels).item()
                 total += data.shape[0]
 
-            return nCorrect / total
+            return n_correct / total
 
-    def trainModel(self, epochs, dataLoaders, with_early_stopping=True):
+    def train_model(self, epochs: int, data_loaders: dict, with_early_stopping=True):
 
         # early stopping params
-        bestValidationAcc = 0
-        patienceLimit = 20
-        patienceCounter = 0
-        needToStop = False
+        best_validation_acc = 0
+        patience_limit = 20
+        patience_counter = 0
+        need_to_stop = False
         best_model_epoch_number = 0
-
         val_accuracies = []
 
         epoch = 0
-        while epoch < epochs and not needToStop:
+        while epoch < epochs and not need_to_stop:
             self.train()
 
-            for data, labels in dataLoaders[TRAIN]:
+            for data, labels in data_loaders[TRAIN]:
                 data, labels = data.to(DEVICE), labels.to(DEVICE)
 
                 self.optimizer.zero_grad()
                 predictions = self(data).to(DEVICE)
-                predictions = predictions.squeeze()
-
                 loss = self.lossFunction(predictions, labels).to(DEVICE)
                 loss.backward()
                 self.optimizer.step()
@@ -166,17 +161,18 @@ class GTSRBModel(nn.Module):
 
             if with_early_stopping:
                 self.eval()
-                valAcc = self.calculateAccuracy(dataLoaders[VALID], with_grad=False)
-                val_accuracies.append(valAcc)
-                if valAcc > bestValidationAcc:
-                    bestValidationAcc = valAcc
+                val_acc = self.calculate_accuracy(data_loaders[VALID], with_grad=False)
+                val_accuracies.append(val_acc)
+                if val_acc > best_validation_acc:
+                    best_validation_acc = val_acc
                     torch.save(self.state_dict(), os.path.join(PATH_TO_MODEL, f'model_{self.modelId}.pth'))
-                    patienceCounter = 0
+                    patience_counter = 0
                     best_model_epoch_number = epoch
                 else:
-                    patienceCounter += 1
-                    if patienceCounter >= patienceLimit:
-                        needToStop = True
+                    patience_counter += 1
+                    if patience_counter >= patience_limit:
+                        need_to_stop = True
+                        print(f'early stopping after {epoch} / {epochs}')
 
             epoch += 1
 
@@ -192,8 +188,8 @@ class GTSRBModel(nn.Module):
         plt.close(fig)
         plt.clf()
 
-        validationAcc = self.calculateAccuracy(dataLoaders[VALID], with_grad=False)
-        testAcc = self.calculateAccuracy(dataLoaders[TEST], with_grad=False)
-        print(f'Validation Accuracy: {(validationAcc * 100):.2f}%')
-        print(f'Test Accuracy: {(testAcc * 100):.2f}%')
+        validation_acc = self.calculate_accuracy(data_loaders[VALID], with_grad=False)
+        test_acc = self.calculate_accuracy(data_loaders[TEST], with_grad=False)
+        print(f'Validation Accuracy: {(validation_acc * 100):.2f}%')
+        print(f'Test Accuracy: {(test_acc * 100):.2f}%')
         print()
